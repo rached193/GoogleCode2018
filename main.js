2c643732f096daa77413af251cb7eaac6e3cca1e
Marcos Allue Visa <marcosalluevisa@hotmail.com>
2018-03-01 21:39:38 +0100

    integracion coche con nueva funcion busca cosas

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ class Car {
 		if (this.step == TIME) {
 			return false;
 		} else {
-			
+
 			if (direction == 'UP') {
 				this.y = this.y - 1;
 			} else if (direction == 'DOWN') {
@@ -27,31 +27,37 @@ class Car {
 			} else if (direction == 'LEFT') {
 				this.x = this.x - 1;
 			}
-			
+
 			this.step = this.step + 1;
-			
+
 			return true;
 		}
 	}
-	
+
 	getTraveller() {
 		if (TRAVELLERS.length > 0) {
-			this.traveller = TRAVELLERS.pop();
+			var index=selectViajero(this);
+			if(index != -1){
+				this.traveller = TRAVELLERS.splice(index, 1)[0];
+			}
+			else{
+				this.traveller = TRAVELLERS.pop();
+			}
 		}
 	}
-	
+
 	goInitPosition() {
 		//movimiento horizontal
 		var hDesp = this.traveller.cordenadas_inicio.x - this.x;
 		var direction = (hDesp > 0) ? 'RIGHT' : 'LEFT';
-		
-		
+
+
 		for (var i = 0; i < Math.abs(hDesp); i++) {
 			if (!this.move(direction)) {
 				break;
 			}
 		}
-		
+
 		//movimiento vertical
 		var vDesp = this.traveller.cordenadas_inicio.y - this.y;
 		var direction = (vDesp > 0) ? 'DOWN' : 'UP';
@@ -61,7 +67,7 @@ class Car {
 			}
 		}
 	}
-	
+
 	goToDestination() {
 		//movimiento horizontal
 		var hDesp = this.traveller.cordenadas_fin.x - this.x;
@@ -79,13 +85,13 @@ class Car {
 				break;
 			}
 		}
-		
+
 		if (this.step <= TIME) {
 			this.hits.push(this.traveller.id);
 			this.traveller = null;
 		}
 	}
-	
+
 	go() {
 		while (this.step < TIME) {
 			this.getTraveller();
@@ -99,8 +105,8 @@ class Car {
 			}
 		}
 	}
-	
-	
+
+
 }
 
 const args = process.argv;
@@ -134,19 +140,18 @@ function initCars() {
 	for (var i = 0; i < CARS; i++) {
 		cars.push(new Car());
 	}
-	
+
 }
 
 function carsGo() {
 	//reparto inicial
 	var indexCar = 0;
-
 	while(TRAVELLERS.length > 0){
 	  cars[indexCar].go();
 	  console.log(cars[indexCar].hits);
 	  indexCar++;
 	}
-	
+
 }
 
 function finish() {
@@ -158,5 +163,37 @@ function finish() {
 				console.log(err)
 			}
 		});
+	}
+}
+function distManhatan(x1,y1,x2,y2){
+	var val= Math.abs(x1-x2) + Math.abs(y1-y2);
+	return val;
+}
+function selectViajero(car){
+	var maxValue = -1;
+	var maxI=-1;
+	for(var i=0;i<TRAVELLERS.length;i++){
+		var value=magicFunction(car,TRAVELLERS[i].cordenadas_inicio,TRAVELLERS[i].cordenadas_fin,TRAVELLERS[i].tiempo_inicio,TRAVELLERS[i].tiempo_fin);
+		if(!value<=0&&value>maxValue){
+			maxValue=value;
+			maxI=i;
+		}
+	}
+
+	return maxI;
+}
+function magicFunction(posIni,posViajero,dest,tmax,tini,tactual){
+	var cosa1=distManhatan(posIni.x,posIni.y,posViajero.x,posViajero.y);
+	var cosa2=distManhatan(posViajero.x,posViajero.y,dest.x,dest.y);
+	var estimTemp=cosa1+cosa2+tactual;
+	if(estimTemp>tmax){
+		return -10000000000000;
+	}
+	else{
+		var bonusPoints=0;
+		if((cosa1+tactual)<=tini){
+			bonusPoints=BONUS - (tini-(cosa1+tactual));
+		}
+		return ((cosa2+bonusPoints)-cosa1);
 	}
 }
