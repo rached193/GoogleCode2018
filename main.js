6f2280adedf0b8704bca6569039d4819b8659dab
rached193 <goldensun193>
2018-03-01 20:43:41 +0100

    Funcion Finish

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /* Dependencias */
-
+var fs = require('fs');
 
 var read = require("./readFile.js");
 
@@ -8,15 +8,16 @@ class Car {
 		this.x = (x) ? x : 0;
 		this.y = (y) ? y : 0;
 		this.step = 0;
-    this.traveller = null;
-    this.hits = [];
-  }
+		this.traveller = null;
+		this.hits = [];
+	}
+
 //MAX_X, MAX_Y
 	move(direction) {
 		if (this.step == TIME) {
 			return false;
 		} else {
-
+			
 			if (direction = 'UP') {
 				this.y = this.y - 1;
 			} else if (direction = 'DOWN') {
@@ -26,62 +27,62 @@ class Car {
 			} else if (direction = 'LEFT') {
 				this.x = this.x - 1;
 			}
-
+			
 			this.step = this.step + 1;
-
+			
 			return true;
 		}
-  }
-
-  getTraveller() {
-    if(TRAVELLERS.length > 0){
-      this.traveller = TRAVELLERS.pop();
-    }
-  }
-
-  goInitPostion(){
-    //movimiento horizontal
-    var hDesp = this.traveller.cordenadas_inicio.x - this.x;
-    var direction = (hDesp > 0) ? 'RIGHT' : 'LEFT';
-    for(var i = 0; i < Math.abs(hDesp); i++){
-      if(!this.move(direction)){
-        break;
-      }
-    }
-    //movimiento vertical
-    var vDesp = this.traveller.cordenadas_inicio.y - this.y;
-    var direction = (hDesp > 0) ? 'DOWN' : 'UP';
-    for(var i = 0; i < Math.abs(vDesp); i++){
-      if(!this.move(direction)){
-        break;
-      }
-    }
-  }
-
-  goToDestination(){
-    //movimiento horizontal
-    var hDesp = this.traveller.cordenadas_fin.x - this.x;
-    var direction = (hDesp > 0) ? 'RIGHT' : 'LEFT';
-    for(var i = 0; i < Math.abs(hDesp); i++){
-      if(!this.move(direction)){
-        break;
-      }
-    }
-    //movimiento vertical
-    var vDesp = this.traveller.cordenadas_fin.y - this.y;
-    var direction = (hDesp > 0) ? 'DOWN' : 'UP';
-    for(var i = 0; i < Math.abs(vDesp); i++){
-      if(!this.move(direction)){
-        break;
-      }
-    }
-
-    if(this.step <= TIME){
-      this.hit = this.hit +1;
-    }
-  }
-
-
+	}
+	
+	getTraveller() {
+		if (TRAVELLERS.length > 0) {
+			this.traveller = TRAVELLERS.pop();
+		}
+	}
+	
+	goInitPostion() {
+		//movimiento horizontal
+		var hDesp = this.traveller.cordenadas_inicio.x - this.x;
+		var direction = (hDesp > 0) ? 'RIGHT' : 'LEFT';
+		for (var i = 0; i < Math.abs(hDesp); i++) {
+			if (!this.move(direction)) {
+				break;
+			}
+		}
+		//movimiento vertical
+		var vDesp = this.traveller.cordenadas_inicio.y - this.y;
+		var direction = (hDesp > 0) ? 'DOWN' : 'UP';
+		for (var i = 0; i < Math.abs(vDesp); i++) {
+			if (!this.move(direction)) {
+				break;
+			}
+		}
+	}
+	
+	goToDestination() {
+		//movimiento horizontal
+		var hDesp = this.traveller.cordenadas_fin.x - this.x;
+		var direction = (hDesp > 0) ? 'RIGHT' : 'LEFT';
+		for (var i = 0; i < Math.abs(hDesp); i++) {
+			if (!this.move(direction)) {
+				break;
+			}
+		}
+		//movimiento vertical
+		var vDesp = this.traveller.cordenadas_fin.y - this.y;
+		var direction = (hDesp > 0) ? 'DOWN' : 'UP';
+		for (var i = 0; i < Math.abs(vDesp); i++) {
+			if (!this.move(direction)) {
+				break;
+			}
+		}
+		
+		if (this.step <= TIME) {
+			this.hit = this.hit + 1;
+		}
+	}
+	
+	
 }
 
 const args = process.argv;
@@ -113,5 +114,17 @@ var cars = [];
 function initCars() {
 	for (var i = 0; i < CARS; i++) {
 		cars.push(new Car());
+	}
+}
+
+function finish() {
+	for (var i = 0; i < CARS; i++) {
+		var current_car = CARS[i].hit;
+		console.log(current_car.stringify());
+		fs.appendFile('out.txt',current_car.length+"\n", function (err) {
+			if (err) {
+				console.log(err)
+			}
+		});
 	}
 }
